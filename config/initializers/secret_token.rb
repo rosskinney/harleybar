24f6215f286939997562225a2fc316e20280df0c
MakerSquare Student <students@themakersquare.com>
2014-01-09 14:10:10 -0600

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Harleybar::Application.config.secret_key_base = '1d6841c336038e13b55ce50813b84461b4b47c203ff5969011b33694e3c336f3a0e90cd6059a0997525e8ad4ee772a940a3f9af685c40fb5e929f2f043d08140'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Harleybar::Application.config.session_store :cookie_store, key: '_harleybar_session'
